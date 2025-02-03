2fb6eec37169ac542782d9fe38ce35989147520f
in-ch <sxin2949@naver.com>
2025-02-03 18:15:33 +0900

    fix: getPackageInfo to fetch version from global package instead of local

--- packages/setup/lib/get-package-info.ts
+++ packages/setup/lib/get-package-info.ts
@@ -1,8 +1,18 @@
+import { execSync } from 'child_process';
 import fs from 'fs-extra';
 import path from 'path';
 
-export function getPackageInfo() {
-  const packageJsonPath = path.join('package.json');
-
-  return fs.readJSONSync(packageJsonPath);
+export function getPackageInfo(packageName = '@in-ch/setup') {
+  try {
+    const globalRoot = execSync('npm root -g', { encoding: 'utf-8' }).trim();
+    const packageJsonPath = path.join(globalRoot, packageName, 'package.json');
+    if (fs.existsSync(packageJsonPath)) {
+      return JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+    } else {
+      throw new Error('package.json not found');
+    }
+  } catch (error) {
+    console.error('Error fetching global package.json:', error);
+    return null;
+  }
 }
