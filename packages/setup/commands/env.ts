13732b6fbb48782faef47ea4d8d60fe83f8f8a8a
in-ch <sxin2949@naver.com>
2025-03-15 09:26:52 +0800

    feat: add input logic key & value to env command

--- packages/setup/commands/env.ts
+++ packages/setup/commands/env.ts
@@ -3,6 +3,7 @@ import { Command } from 'commander';
 import fs, { existsSync } from 'fs';
 import versionCheckAndUpdate from 'lib/version-update.js';
 import path from 'path';
+import { confirm, input } from '@inquirer/prompts';
 
 export const envCli = new Command()
   .command('env')
@@ -18,8 +19,37 @@ export const envCli = new Command()
     } else {
       try {
         const rootDir = process.cwd();
-        fs.writeFileSync(path.join(rootDir, '.env'), '', 'utf-8');
+        const envFilePath = path.join(rootDir, '.env');
+
+        fs.writeFileSync(envFilePath, '', 'utf-8');
         console.log('Created .env file successfully');
+
+        const addKeyValuePair = async () => {
+          const response = await confirm({
+            message: 'Would you like to add a key to the .env file?',
+          });
+
+          if (response) {
+            const key = await input({
+              message: 'Enter the key:',
+              default: '',
+            });
+
+            const value = await input({
+              message: 'Enter the value:',
+              default: '',
+            });
+
+            const envContent = `${key}=${value}\n`;
+            fs.appendFileSync(envFilePath, envContent, 'utf-8');
+            console.log(`Added ${key}=${value} to .env`);
+
+            addKeyValuePair();
+          } else {
+            console.log('Finished adding key-value pairs.');
+          }
+        };
+        addKeyValuePair();
       } catch (e) {
         console.error('🥲 Failed to create env file');
       }
